874c641ad0b825556cbd261ae70ac26f76fb1235
justinp9123 <justinp588@nycstudents.net>
2025-10-02 19:05:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Justin Perez"; // update the part BETWEEN the "quotes"
+let userName = "justinp9123"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
